0ab706ee7c7435da5fcdd4265caf6ae30861ef3b
Sergey Skorohod <serik.plus.plus@gmail.com>
2022-06-17 14:27:19 +0300

    contact-us page

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,11 @@ import sliders from './modules/sliders.js'
 import timeline from './modules/timeline.js'
 import meetTeam from './modules/meetTeam.js'
 import subPageSlider from './modules/sub-page.js'
+import {
+	dropdawnTranslateFrom,
+	dropdawnTranslateTo,
+	dropdawnTranslateFile,
+} from './modules/getquote.js'
 
 flsFunctions.isWebp()
 header.collapse()
@@ -20,3 +25,7 @@ try {
 try {
 	subPageSlider()
 } catch (error) {}
+
+dropdawnTranslateFrom()
+// dropdawnTranslateFile()
+// dropdawnTranslateTo()
